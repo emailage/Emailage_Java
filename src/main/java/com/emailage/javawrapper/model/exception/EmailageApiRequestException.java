95c134065e6b2ea86467bc9b9d9a248643d9f263
Martin Duncan <martin.duncan@emailage.com>
2019-02-14 18:01:39 -0700

    Move to Exceptions package

--- src/main/java/com/emailage/javawrapper/model/exception/EmailageApiRequestException.java
+++ src/main/java/com/emailage/javawrapper/model/exception/EmailageApiRequestException.java
@@ -1,4 +1,4 @@
-package com.emailage.javawrapper.model;
+package com.emailage.javawrapper.model.exception;
 
 public class EmailageApiRequestException extends Exception {
     public EmailageApiRequestException(String message, Exception e){super(message,e);}
--- src/main/java/com/emailage/javawrapper/model/exception/EmailageParameterException.java
+++ src/main/java/com/emailage/javawrapper/model/exception/EmailageParameterException.java
@@ -1,4 +1,4 @@
-package com.emailage.javawrapper.model;
+package com.emailage.javawrapper.model.exception;
 
 public class EmailageParameterException extends Exception {
     public EmailageParameterException(String message, Exception ex){super(message,ex);}
