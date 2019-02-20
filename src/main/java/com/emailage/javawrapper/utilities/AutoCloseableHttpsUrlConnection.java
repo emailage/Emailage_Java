4abf505767a7157f115f2f882e01cd18f13cff21
Martin Duncan <martin.duncan@emailage.com>
2019-02-20 10:48:54 -0700

    update javadoc documentation

--- src/main/java/com/emailage/javawrapper/utilities/AutoCloseableHttpsUrlConnection.java
+++ src/main/java/com/emailage/javawrapper/utilities/AutoCloseableHttpsUrlConnection.java
@@ -2,6 +2,9 @@ package com.emailage.javawrapper.utilities;
 
 import javax.net.ssl.HttpsURLConnection;
 
+/**
+ * Simple wrapper for a connection to disconnect the connection when the resource block is exited.
+ */
 public class AutoCloseableHttpsUrlConnection implements AutoCloseable {
     private HttpsURLConnection connection;
     public AutoCloseableHttpsUrlConnection(HttpsURLConnection connection)
