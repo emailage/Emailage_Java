28021a5d02e455c170703e6373e63b4d25e84af8
Martin Duncan <martin.duncan@emailage.com>
2021-01-27 14:58:48 -0700

    Changed method getOutputStream -> doOAuth2Request to make things less confusing. Use Helper to reduce duplicate code and make things more readable. Added support or refresh token usage.

--- src/main/java/com/emailage/javawrapper/utilities/OAuth2Wrapper.java
+++ src/main/java/com/emailage/javawrapper/utilities/OAuth2Wrapper.java
@@ -7,9 +7,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 
 import javax.net.ssl.HttpsURLConnection;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
@@ -48,6 +45,7 @@ public class OAuth2Wrapper {
 
 
     private static final String tokenForm = "grant_type=client_credentials&client_id=%s&client_secret=%s";
+    private static final String refreshForm = "grant_type=token_refresh&client_id=%s&refresh_token=%s";
 
     protected OAuth2Wrapper(String accountToken, String accountSecret, URL tokenUrl){
         this.accountSecret = accountSecret;
@@ -57,44 +55,52 @@ public class OAuth2Wrapper {
         this.token = new OAuth2Token();
     }
 
-    public OutputStream getOutputStream(HttpsURLConnection conn, int postDataLength, String charset) throws Exception {
+    public String doOAuth2Request(URL url, String form) throws Exception {
 
-        if( expiration.isBefore(LocalDateTime.now())){
+        if(expiration.isBefore(LocalDateTime.now())){
             token = getAccessToken(token, accountToken, accountSecret, this.tokenUrl);
             expiration = LocalDateTime.now().plusSeconds(token.getAccessExpiration());
         }
 
-        conn.setRequestProperty("Content-Language", "en-US");
-        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=" + charset);
-        conn.setRequestProperty("Accept-Charset", charset);
-        conn.setRequestProperty("Authorization","Bearer " + token.getAccessToken());
-        conn.setRequestProperty("Content-Length", Integer.toString(postDataLength));
-        conn.setDoOutput(true);
+        String answer = null;
+        byte[] body = form.getBytes(StandardCharsets.UTF_8);
+        HttpsURLConnection conn = HttpHelper.getHttpsURLConnection(url);
+        try (AutoCloseable conc = new AutoCloseableHttpsUrlConnection(conn)) {
+            answer = HttpHelper.PostRequest(body, conn);
+        }
 
-        return conn.getOutputStream();
-    }
-
-    public InputStream getInputStream(HttpsURLConnection conn) throws IOException {
-        return conn.getInputStream();
+        return answer;
     }
 
     private OAuth2Token getAccessToken(OAuth2Token token, String accountToken, String accountSecret, URL tokenUrl) throws Exception {
 
-        String form = String.format(tokenForm, accountSecret, accountToken);
-        byte[] body = form.getBytes(StandardCharsets.UTF_8);
-        int bodySize = body.length;
-
         String answer = null;
-        HttpsURLConnection conn = HttpHelper.getHttpsURLConnection(tokenUrl);
-        try(AutoCloseable conc = new AutoCloseableHttpsUrlConnection(conn)) {
+        try {
+            HttpsURLConnection conn = HttpHelper.getHttpsURLConnection(tokenUrl);
+            try (AutoCloseable conc = new AutoCloseableHttpsUrlConnection(conn)) {
 
-            conn.setRequestProperty("Content-Language", "en-US");
-            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
-            conn.setRequestProperty("Accept-Charset", StandardCharsets.UTF_8.name());
-            answer = HttpHelper.PostRequest(body, conn);
+                String form = null;
+                if(token.getAccessToken() != null){
+                    form = String.format(refreshForm, accountSecret, token.getRefreshToken());
+                } else {
+                    form = String.format(tokenForm, accountSecret, accountToken);
+                }
+
+                byte[] body = form.getBytes(StandardCharsets.UTF_8);
+                answer = HttpHelper.PostRequest(body, conn);
+            }
+        } catch (Exception ex){
+            // refresh token has expired so set the refresh token to null and try
+            // using the full credentials.
+            if(token.getRefreshToken() != null) {
+                token.setRefreshToken(null);
+                getAccessToken(token, accountToken, accountSecret, tokenUrl);
+            } else {
+                throw ex;
+            }
         }
 
-        OAuth2Token result = InstanceHolder.mapper.readValue(answer, OAuth2Token.class);
+        OAuth2Token result = InstanceHolder.mapper.readValue(answer,OAuth2Token.class);
         return result;
     }
 }
